56b9315394795d8af40609ce26b09a671baa014b
Zulal Akbulut <zulalakbulut2516@gmail.com>
2023-12-24 22:04:11 +0100

    ZulalV1

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -1,80 +1,89 @@
 package utilities;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.List;
+import java.util.NoSuchElementException;
 
 public class Driver {
-    /*
-    POM'de Driver icin TestBase class'ina extends etmek yerine
-    Driver class'indan static method'lar kullanarak
-    driver olusturup, ilgili ayarlarin yapilmasi
-    ve en sonda driver'in kapatilmasi tercih edilmistir
+    //create a driver instance
+    public static WebDriver driver;
+    public static int timeout = 5;
 
-    POM'de Driver class'indaki getDriver()'nin obje olusturularak kullanilmasini
-    engellemek icin
-    Singleton Pattern : tekli kullanim, bir class'in farkli class'lardan
-    obje olusturularak kullanimini engellemek icin kullanilir.
-
-    Bunu saglamak icin yapmamiz gereken sey oldukca basit
-    obje olusturmak icin kullanilan constructor'i private yaptiginizda
-    baska class'larda Driver class'indan obje olusturulmasi mumkun OLAMAZ
-     */
-
-    private Driver(){
-
+    //What?=>It is just to create, initialize the driver instance.(Singleton driver)
+    //Why?=>We don't want to create and initialize the driver when we don't need
+    //We will create and initialize the driver when it is null
+    //We can use Driver class with different browser(chrome,firefox,headless)
+    private Driver() {
+        //we don't want to create another abject. Singleton pattern
     }
-    static WebDriver driver;
 
-    public static WebDriver getDriver(){
+    //to initialize the driver we create a static method
+    public static WebDriver getDriver() {
+        //create the driver if and only if it is null
+        if (driver == null) {
 
-        if(driver==null) {
-            switch(ConfigReader.getProperty("browser")){
-                case "chrome" :
-                    WebDriverManager.chromedriver().setup();
-                    ChromeOptions chromeOptions = new ChromeOptions();
-                    chromeOptions.addArguments("--remote-allow-origins=*");
-                    driver = new ChromeDriver(chromeOptions);
-                    break;
-                case "safari" :
-                    WebDriverManager.safaridriver().setup();
-                    driver=new SafariDriver();
-                    break;
-                case "firefox" :
-                    WebDriverManager.firefoxdriver().setup();
-                    driver=new FirefoxDriver();
-                    break;
-                case "edge" :
-                    WebDriverManager.edgedriver().setup();
-                    driver=new EdgeDriver();
-                    break;
-                case "headless-chrome" :
-                    WebDriverManager.chromedriver().setup();
-                    driver = new ChromeDriver(new ChromeOptions().setHeadless(true));
-                    break;
-                default:
-                    WebDriverManager.chromedriver().setup();
-                    driver = new ChromeDriver();
-
-
+            String browser = ConfigReader.getProperty("browser");
+            if ("chrome".equals(browser)) {
+                driver = new ChromeDriver();
+            } else if ("firefox".equals(browser)) {
+                driver = new FirefoxDriver();
+            } else if ("ie".equals(browser)) {
+                driver = new InternetExplorerDriver();
+            } else if ("safari".equals(browser)) {
+                driver = new SafariDriver();
+            } else if ("chrome-headless".equals(browser)) {
+                //    driver = new ChromeDriver(new ChromeOptions().setHeadless(true));
             }
-
-            driver.manage().window().maximize();
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         }
+        //    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
         return driver;
     }
 
     public static void closeDriver() {
-        if (driver != null) {
-            driver.close();
-            driver=null;
+        if (driver != null) {//if the driver is pointing chrome
+            driver.quit();//quit the driver
+            driver = null;//set it back to null to make sure driver is null
+            // so I can initialize it again
+            //This is important especially you do cross browser testing(testing with
+            // multiple browser like chrome, firefox, ie etc.)
         }
     }
+
+    //    Driver.selectByVisibleText(dropdown element, "CHECKING-91303-116.98$")
+    public static void selectByVisibleText(WebElement element, String text) {
+        Select objSelect = new Select(element);
+        objSelect.selectByVisibleText(text);
+    }
+    //    Parameter1 : WebElement
+//    Parameter2:  int
+//    Driver.selectByIndex(dropdown element, 1)
+    public static void selectByIndex(WebElement element, int index) {
+        Select objSelect = new Select(element);
+        objSelect.selectByIndex(index);
+    }
+    //    Parameter1 : WebElement
+//    Parameter2:  String
+//    Driver.selectByIndex(dropdown element, "91303")
+    public static void selectByValue(WebElement element, String value) {
+        Select objSelect = new Select(element);
+        List<WebElement> elementCount = objSelect.getOptions();
+        objSelect.selectByValue(value);
+        System.out.println("number of elements: " + elementCount.size());
+    }
+
+
 }
