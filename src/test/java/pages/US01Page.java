dcea22f9d6a5f61c166273e438a12e1de220e45b
Zulal Akbulut <zulalakbulut2516@gmail.com>
2023-12-26 23:47:30 +0100

    ZulalV2

--- src/test/java/pages/US01Page.java
+++ src/test/java/pages/US01Page.java
@@ -1,10 +1,17 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
 public class US01Page {
     public US01Page() {
         PageFactory.initElements(Driver.getDriver(),this);}
+
+    @FindBy(xpath = "//*[@title='ACIBADEM ONLINE'] ")
+    public WebElement acibademOnlineButonu;
+
+
 
 }
--- src/test/java/stepDefinitions/US01StepDefinitions.java
+++ src/test/java/stepDefinitions/US01StepDefinitions.java
@@ -1,11 +1,22 @@
 package stepDefinitions;
 
+import io.cucumber.java.en.*;
 import pages.US01Page;
+import utilities.ConfigReader;
+import utilities.Driver;
 
 public class US01StepDefinitions {
 
     US01Page us01Page = new US01Page();
 
+    @Given("Kullanici acibadem anasayfaya gider")
+    public void kullanici_acibadem_anasayfaya_gider() {Driver.getDriver().get(ConfigReader.getProperty("acibademUrl"));
+
+    }
 
 
+    @Then("Kullanici uye girisi yapar")
+    public void kullaniciUyeGirisiYapar() {
+        us01Page.acibademOnlineButonu.click();
+    }
 }
