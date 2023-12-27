ff99ee21931e8840c017b390aecd9a8a266d74a3
safiyeCK <safiyekara46@gmail.com>
2023-12-27 21:34:48 +0100

    Safiye-V2

--- src/test/java/pages/US09Page.java
+++ src/test/java/pages/US09Page.java
@@ -1,10 +1,23 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
 public class US09Page {
     public US09Page() {
         PageFactory.initElements(Driver.getDriver(),this);}
+
+    //i[@class='icon route-way']  yoltarifi
+
+    @FindBy(xpath = "//i[@class='icon route-way']")
+    public WebElement yoltarifi;
+
+     //  (//span[.='Tümünü kabul et'])[1]  google tumunukabul et butonu
+
+    @FindBy(xpath = "(//span[.='Tümünü kabul et'])[1]")
+    public WebElement tumunuKabulEtGoogle;
+
 
 }
--- src/test/java/stepDefinitions/US09StepDefinitions.java
+++ src/test/java/stepDefinitions/US09StepDefinitions.java
@@ -1,11 +1,31 @@
 package stepDefinitions;
 
 
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
 import pages.US09Page;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
 
 public class US09StepDefinitions {
-    US09Page us09Page = new US09Page();
+    US09Page page=new US09Page();
 
-
-
+    @Then("Yol Tarifi Alın linkinin tiklanabilir oldugunu dogrular")
+    public void yolTarifiAlınLinkininTiklanabilirOldugunuDogrular() {
+        ReusableMethods.scroll(page.yoltarifi);
+        Assert.assertTrue(page.yoltarifi.isDisplayed()&& page.yoltarifi.isEnabled());
+        ReusableMethods.waitFor(2);
+        ReusableMethods.click(page.yoltarifi);
+        ReusableMethods.waitFor(2);
+    }
+    @Then("Yol Tarifi Alın linkinin Google Maps'e yonlendirdigini dogrular")
+    public void yolTarifiAlınLinkininGoogleMapsEYonlendirdiginiDogrular() {
+        ReusableMethods.switchToWindow(1);
+        Assert.assertTrue( Driver.getDriver().getCurrentUrl().contains("google"));
+        ReusableMethods.scroll(page.tumunuKabulEtGoogle);
+        ReusableMethods.click(page.tumunuKabulEtGoogle);
+        String url=Driver.getDriver().getCurrentUrl();
+        Assert.assertTrue(url.contains("maps"));
+    }
 }
