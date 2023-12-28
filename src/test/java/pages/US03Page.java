9c767c98732d73e18d502559e4f65972505b37c3
kkbraslnn <kkaratash@icloud.com>
2023-12-28 11:11:33 +0100

    V1Kubra

--- src/test/java/pages/US03Page.java
+++ src/test/java/pages/US03Page.java
@@ -1,10 +1,33 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
 public class US03Page {
     public US03Page() {
         PageFactory.initElements(Driver.getDriver(),this);}
+
+    @FindBy (xpath = "(//*[@title='DOKTORLAR'])[1]")
+    public WebElement doktorlarSekmesi;
+
+    @FindBy (xpath = "//span[text()='Doktorlar']")
+    public WebElement doktorlarSayfasi;
+    //span[@class='m-nav__link-text']
+
+    @FindBy (id="location")
+    public WebElement DrLokasyonSecme;
+
+    @FindBy (id="unit")
+    public WebElement DrTibbiBirimSecme;
+
+    @FindBy( id = "doctorType")
+    public WebElement DrUnvanSecme;
+
+    @FindBy (xpath = "(//img[@class='doctor-lazy-load-img-cancelled'])[2]")
+    public WebElement DoktorSecme;
+
+
 
 }
--- src/test/java/pages/US07Page.java
+++ src/test/java/pages/US07Page.java
@@ -1,10 +1,40 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
 public class US07Page {
     public US07Page() {
+
         PageFactory.initElements(Driver.getDriver(),this);}
+
+    @FindBy (xpath = "(//*[@title='İLETİŞİM'])[1]")
+    public WebElement iletisimSekmesi;
+    //*[@id="active"]/a
+    @FindBy(xpath = "//a[.='İLETİŞİM']")
+    public  WebElement homePageIletisimButton;
+
+    @FindBy (className = "scrollContent livechat")
+    public WebElement canlidestek;
+
+
+    @FindBy (xpath = "(//*[@id='29val-field_1.3.10--input'])")
+    public WebElement namebutton;
+
+
+    @FindBy (xpath = "(//*[@id='29val-field_1.3.10--input'])")
+    public WebElement emailbutton;
+
+
+    @FindBy (xpath = "(//*[@id='31val-field_1.3.10--input'])")
+    public WebElement messagebutton;
+
+
+    @FindBy (className = "Start chat")
+    public WebElement startchatbutton;
+
+
 
 }
