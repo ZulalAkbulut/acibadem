b0a14fb28eeea8412ad8ec4068a80dd0a410e3b7
adali24 <sadecehamit@gmail.com>
2023-12-28 23:09:02 +0100

    Hamitv5

--- src/test/java/stepDefinitions/US08StepDefinitions.java
+++ src/test/java/stepDefinitions/US08StepDefinitions.java
@@ -5,11 +5,9 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.eo.Se;
+import org.apache.commons.io.FileUtils;
 import org.junit.Assert;
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.Select;
 import pages.US08Page;
 import utilities.ConfigReader;
@@ -17,8 +15,11 @@ import utilities.Driver;
 import utilities.ReusableMethods;
 
 import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
 import java.util.Set;
 
+import static org.junit.Assert.assertEquals;
 import static utilities.Driver.driver;
 
 public class US08StepDefinitions {
@@ -34,95 +35,70 @@ public class US08StepDefinitions {
     public void kullaniciMenuButonunuTiklar() {
         us08Page.menu.click();
     }
-
-    @Then("Kullanici acilan menuyu asagi kaydirir")
-    public void kullaniciAcilanMenuyuAsagiKaydirir() {
-
-    }
-
     @Then("Kullanici FOR INTERNATIONAL PATIENTS butonu tiklar")
     public void kullaniciButonuTiklar() {
         us08Page.internationalPatients.click();
         String ilkSayfaHandle = driver.getWindowHandle();
         Set<String> pencereler = driver.getWindowHandles();
         for (String w:pencereler) {
-            System.out.println(w);
             if (!w.equals(ilkSayfaHandle)){
                 driver.switchTo().window(w);
             }
         }
-
     }
-
-    @Then("Kullanici acilan sayfada istenen bilgileri girer")
-    public void kullaniciAcilanSayfadaIstenenBilgileriGirer() {
-     //   Driver.getDriver().get(ConfigReader.getProperty("acibademinternationalUrl"));
-    }
-
     @Then("Kullanici acilan cookies kapatir")
     public void kullaniciAcilanCookiesKapatir() {
-        // ReusableMethods.hooverByJS(us08Page.cookies);
-       // us08Page.cookies.click();
         ReusableMethods.click(us08Page.cookies);
     }
-
     @Then("Kullanici {string} kutusuna isim girer")
     public void kullaniciKutusunaIsimGirer(String str) {
         us08Page.name.sendKeys("Tom");
     }
-
     @Then("Kullanici {string} kutusuna soyisim girer")
     public void kullaniciKutusunaSoyisimGirer(String str) {
-        us08Page.lastName.sendKeys("Hanks");
+        us08Page.lastName.sendKeys("Kankas");
     }
-
     @Then("Kullanici {string} kutusuna mail adresi girer")
     public void kullaniciKutusunaMailAdresiGirer(String str) {
-        us08Page.email.sendKeys("tomkank8s@yahoo.com");
+        us08Page.email.sendKeys("tomkanka8s@yahoo.com");
     }
-
     @Then("Kullanici {string} kutusuna telefon numarasi girer")
     public void kullaniciKutusunaTelefonNumarasiGirer(String str) {
         us08Page.phoneNumber.sendKeys("98760543");
     }
-
     @Then("Kullanci Country menusunden ulke secer")
     public void kullanciCountryMenusundenUlkeSecer() {
         Select select = new Select(us08Page.country);
         select.selectByValue("154");
         //value="154" Nederlands
     }
-
     @Then("Kullanici Treatment Units menusunden bolum secer")
     public void kullaniciTreatmentUnitsMenusundenBolumSecer() {
         Select select = new Select(us08Page.treatmentUnits);
         select.selectByVisibleText("Dentistry");
     }
-
     @Then("Kullanici Message kutusuna mesaji girer")
     public void kullaniciMessageKutusunaMesajiGirer() {
         us08Page.message.sendKeys("I want to be treated ");
     }
-
     @Then("Kullanici Checkbox u isaretler")
     public void kullaniciCheckboxUIsaretler() {
         us08Page.checkBox.click();
     }
+  @Then("Kullanici Send butonunu tiklar")
+  public void kullaniciSendButonunuTiklar() {
+      us08Page.sendButton.click();
+  }
 
-//  @Then("Kullanici Send butonunu tiklar")
-//  public void kullaniciSendButonunuTiklar() {
-//      us08Page.sendButton.click();
-//  }
-
-//  @Then("Kayit yapildigini dogrula")
-//  public static void kayitYapildiginiDogrula() {
-//   String alertyazisi= Driver.getDriver().switchTo().alert().getText();
-//   Assert.assertEquals(alertyazisi.);
-
-//  }
-
-
-
+  @Then("Kayit yapildigini dogrula")
+  public static void kayitYapildiginiDogrula() {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      System.out.println("allert mesaji "+alertText);
+      String expectedAlertText = "Thank You. We have received your message.";
+      assertEquals(expectedAlertText, alertText);
+      alert.accept();
+  }
     @Then("Kullanici {int} saniye bekler")
     public void kullaniciSaniyeBekler(int str) throws InterruptedException {
         Thread.sleep(2000);
@@ -142,7 +118,9 @@ public class US08StepDefinitions {
 
     @Then("Hatayi dogrula")
     public void hatayiDogrula() {
+
     }
+
 
 
 }
