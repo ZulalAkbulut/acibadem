cddccabe65981e4ffb55e53be2732bd19d12ce71
Zulal Akbulut <zulalakbulut2516@gmail.com>
2023-12-28 21:45:45 +0100

    ZulalV3

--- src/test/java/pages/US01Page.java
+++ src/test/java/pages/US01Page.java
@@ -12,6 +12,58 @@ public class US01Page {
     @FindBy(xpath = "//*[@title='ACIBADEM ONLINE'] ")
     public WebElement acibademOnlineButonu;
 
+    @FindBy(id="mat-input-0")
+    public WebElement emailButonu;
+
+    @FindBy(xpath = "//*[@id='mat-input-1']")
+    public WebElement sifreButonu;
+
+    @FindBy(xpath = "//*[@class='login-button mat-flat-button mat-primary']")
+    public WebElement uyelikGirisButonu;
+
+    @FindBy(xpath = "//*[@class='hospital-appointment-btn mat-stroked-button mat-basic']")
+    public WebElement randevularButonu;
+
+    @FindBy(xpath = "(//*[@class='mat-form-field-infix'])[2]")
+    public WebElement hastanelerButon;
+
+    @FindBy(xpath = "(//*[@class='mat-option-text'])[1]")
+    public WebElement hastaneIsmi;
+
+    @FindBy(xpath = "(//*[@class='mat-form-field-infix'])[3]")
+    public WebElement servislerButonu;
+
+    @FindBy(xpath = "(//*[@class='mat-option-text'])[6]")
+    public WebElement servisIsmi;
+
+    @FindBy(xpath = "(//*[@class='mat-form-field-infix'])[4]")
+    public WebElement doktorButonu;
+
+    @FindBy(xpath = "//*[@class='mat-option-text']")
+    public WebElement doktorIsmi;
+
+    @FindBy(xpath = "//*[@class='fc-icon fc-icon-right-single-arrow']")
+    public WebElement ileriTarihIkonu;
+
+    @FindBy(xpath = "(//*[@class='fc-content'])[9]")
+    public WebElement tarihVeSaat;
+
+    @FindBy(xpath = "//*[@class='appointmentConfirmButton mat-flat-button mat-primary']")
+    public WebElement randevuOnayButonu;
+
+    @FindBy(xpath = "//*[@class='mat-dialog-title ng-star-inserted']")
+    public WebElement randevuAlinmistirYazisi;
+
+    @FindBy(xpath = "//span[text()='Randevularım']")
+    public WebElement randevuOnaySonrasiRandevularimButonu;
+
+    @FindBy(xpath = "//span[text()='Randevuyu İptal Et']")
+    public WebElement randevuIptalButonu;
+
+    @FindBy(xpath = "//span[text()='Evet']")
+    public WebElement iptalEvetButonu;
+
+
 
 
 }
--- src/test/java/stepDefinitions/US02StepDefinitions.java
+++ src/test/java/stepDefinitions/US02StepDefinitions.java
@@ -1,10 +1,40 @@
 package stepDefinitions;
+import io.cucumber.java.en.*;
+import org.junit.Assert;
 import pages.US02Page;
+
+import static utilities.Driver.driver;
 
 public class US02StepDefinitions {
 
     US02Page us02Page = new US02Page();
 
+    @Then("Kulanici anasayfadaki hastaneler butonunu tiklar")
+    public void kulanici_anasayfadaki_hastaneler_butonunu_tiklar() {
+        us02Page.hastanelerButonu.click();
+    }
 
 
+    @And("Kullanici istedigi lokasyon turunu secer")
+    public void kullaniciIstedigiLokasyonTurunuSecer() throws InterruptedException {
+        if (!us02Page.lokasyonHastaneButonu.isSelected()){
+            us02Page.lokasyonHastaneButonu.click();
+        }
+
+        Thread.sleep(2000);
+    }
+
+    @And("Kullanici aradigi hastaneyi secer")
+    public void kullaniciAradigiHastaneyiSecer() {
+        us02Page.arananHastane.click();
+
+    }
+
+    @And("Kullanici hastane bilgilerini gorur")
+    public void kullaniciHastaneBilgileriniGorur() {
+        String istenenKelime="Adana Hastanesi";
+        String actualTitle=driver.getTitle();
+        Assert.assertTrue(actualTitle.contains(istenenKelime));
+
+    }
 }
